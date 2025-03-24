ea47c385e8e471230026c65e1adce15a820972c5
Miguel Cabeza <migcab86@gmail.com>
2025-03-24 16:24:25 +0000

    Creo "HotelRate" representa las tarifas de hotel

---
+++ xotelo-feeder/src/main/java/dacd/cabeza/model/HotelRate.java
@@ -0,0 +1,18 @@
+package dacd.cabeza.model;
+
+public class HotelRate {
+	private String code;
+	private String name;
+	private double rate;
+
+	// Getters y Setters
+	public String getCode() { return code; }
+	public void setCode(String code) { this.code = code; }
+
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+
+	public double getRate() { return rate; }
+	public void setRate(double rate) { this.rate = rate; }
+}
+
