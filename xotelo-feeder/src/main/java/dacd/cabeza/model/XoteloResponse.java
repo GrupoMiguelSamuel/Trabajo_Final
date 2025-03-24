dec93b698414ec63a5eae773361fe3a322990e23
Miguel Cabeza <migcab86@gmail.com>
2025-03-24 16:24:55 +0000

    Creo "XoteloResponse" modela la respuesta de la API de Xotelo

---
+++ xotelo-feeder/src/main/java/dacd/cabeza/model/XoteloResponse.java
@@ -0,0 +1,35 @@
+package dacd.cabeza.model;
+
+import java.util.List;
+
+public class XoteloResponse {
+	private String error;
+	private long timestamp;
+	private Result result;
+
+	// Getters y Setters
+	public String getError() { return error; }
+	public void setError(String error) { this.error = error; }
+
+	public long getTimestamp() { return timestamp; }
+	public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+
+	public Result getResult() { return result; }
+	public void setResult(Result result) { this.result = result; }
+
+	public static class Result {
+		private String chk_in;
+		private String chk_out;
+		private List<HotelRate> rates;
+
+		// Getters y Setters
+		public String getChk_in() { return chk_in; }
+		public void setChk_in(String chk_in) { this.chk_in = chk_in; }
+
+		public String getChk_out() { return chk_out; }
+		public void setChk_out(String chk_out) { this.chk_out = chk_out; }
+
+		public List<HotelRate> getRates() { return rates; }
+		public void setRates(List<HotelRate> rates) { this.rates = rates; }
+	}
+}
